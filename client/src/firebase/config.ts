cdf806d3a36afaa3b427b927a7b56ddcdc1bb206
AlexMarinkovich <alex-989@live.com>
2025-08-23 23:12:13 -0400

    firebase thumbnail implementation

--- client/src/firebase/config.ts
+++ client/src/firebase/config.ts
@@ -11,7 +11,5 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-console.log("Firebase config:", firebaseConfig);
-
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
